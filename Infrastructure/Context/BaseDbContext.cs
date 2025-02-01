4439c02fa6cf0ec404d5fab4dd2b5fd6e919cf05
mohamad itani <mohammad.adnan.itani@gmail.com>
2025-02-01 22:04:43 +0200

    Added Base DB

---
+++ Infrastructure/Context/BaseDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Infrastructure.Identity.Models;
+using Infrastructure.Tenancy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public abstract class BaseDbContext : MultiTenantIdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
+    {
+        protected BaseDbContext(IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantInfoContextAccessor, DbContextOptions options) : base(tenantInfoContextAccessor, options)
+        {
+        }
+
+
+    }
+}
---
+++ Infrastructure/Identity/Models/ApplicationRoleClaim.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity.Models
+{
+    public class ApplicationRoleClaim : IdentityRoleClaim<string>
+    {
+        public string Description { get; set; }
+        public string Group { get; set; }
+    }
+}
