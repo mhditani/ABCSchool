7397b53b1fecd6ca214922504664887011318448
mohamad itani <mohammad.adnan.itani@gmail.com>
2025-02-03 22:45:04 +0200

    Added Base DB Context

--- Infrastructure/Context/BaseDbContext.cs
+++ Infrastructure/Context/BaseDbContext.cs
@@ -8,17 +8,34 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Context
 {
+
     public abstract class BaseDbContext : MultiTenantIdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>, ApplicationRoleClaim, IdentityUserToken<string>>
     {
+
+        private new ABCSchoolTenantInfo TenantInfo { get; set; }
+
         protected BaseDbContext(IMultiTenantContextAccessor<ABCSchoolTenantInfo> tenantInfoContextAccessor, DbContextOptions options) : base(tenantInfoContextAccessor, options)
         {
+            TenantInfo = tenantInfoContextAccessor.MultiTenantContext.TenantInfo;
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
 
+            if (!string.IsNullOrEmpty(TenantInfo?.ConnectionString))
+            {
+                optionsBuilder.UseSqlServer(TenantInfo.ConnectionString, options =>
+                {
+                    options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+                });
+            }
+        }
     }
 }
